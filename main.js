e45447d55457251b1a0a02db3974b2faf704c184
Dubovski <dubouski.sa@gmail.com>
2022-05-23 21:37:49 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,107 @@
+'use strict'
+
+// let a = Math.floor((Math.random() - 0.5) * 200);
+// console.log(a);
+// let b = Math.floor((Math.random() - 0.5) * 200);
+// console.log(b);
+// if (a >= 0 && b >= 0) {
+//     console.log("+ " + (a - b));
+// } else if (a < 0 && b < 0) {
+//     console.log("* " + (a * b));
+// } else {
+//     console.log("- " + (a + b));
+// };
+
+
+// let a = Math.floor(Math.random() * 15);
+// switch (a) {
+//     case 1:
+//         console.log(a++);
+//     case 2:
+//         console.log(a++);
+//     case 3:
+//         console.log(a++);
+//     case 4:
+//         console.log(a++);
+//     case 5:
+//         console.log(a++);
+//     case 6:
+//         console.log(a++);
+//     case 7:
+//         console.log(a++);
+//     case 8:
+//         console.log(a++);
+//     case 9:
+//         console.log(a++);
+//     case 10:
+//         console.log(a++);
+//     case 11:
+//         console.log(a++);
+//     case 12:
+//         console.log(a++);
+//     case 13:
+//         console.log(a++);
+//     case 14:
+//         console.log(a++);
+//     case 15:
+//         console.log(a);
+// }
+
+// let arg1 = Math.round(Math.random() * 100);
+// let arg2 = Math.round(Math.random() * 100);
+// console.log(arg1);
+// console.log(arg2);
+// function sum(a, b) {
+//     return a + b;
+// };
+
+// function dif(a, b) {
+//     return a - b;
+// };
+
+// function mul(a, b) {
+//     return a * b;
+// };
+// function div(a, b) {
+//     return Math.round((a / b) * 100) / 100;
+// };
+
+// console.log('сумма ' + sum(arg1, arg2));
+// console.log('разность ' + dif(arg1, arg2));
+// console.log('произведение ' + mul(arg1, arg2));
+// console.log('деление ' + div(arg1, arg2));
+
+// function mathOperation(n1, n2, operation) {
+//     switch (operation) {
+//         case '+':
+//             return sum(n1, n2);
+//         case '-':
+//             return dif(n1, n2);
+//         case '*':
+//             return mul(n1, n2);
+//         case '/':
+//             return div(n1, n2);
+//     }
+// }
+
+// console.log('+ ' + mathOperation(arg1, arg2, '+'));
+// console.log('- ' + mathOperation(arg1, arg2, '-'));
+// console.log('* ' + mathOperation(arg1, arg2, '*'));
+// console.log('/ ' + mathOperation(arg1, arg2, '/'));
+
+
+
+// function power(val, pow) {
+//     if (pow !== 1) {
+//         return val * power(val, pow - 1)
+//     }
+//     return val
+// }
+// console.log(power(2, 3));
+
+
+// function min(a, b) {
+//     return (a < b ? a : b);
+// }
+
+// console.log(min(arg1, arg2));
---
+++ main3.js
@@ -0,0 +1,102 @@
+'use strict'
+
+// Напишите код, выполнив задание из каждого пункта отдельной строкой:
+// Создайте пустой объект user.
+// Добавьте свойство name со значением John.
+// Добавьте свойство surname со значением Smith.
+// Измените значение свойства name на Pete.
+// Удалите свойство name из объекта.
+// let user = {};
+// console.log(user);
+// user.name = 'John';
+// console.log(user);
+// user.surname = 'Smith';
+// user.name = 'Pete';
+// console.log(user);
+// delete user.name;
+// console.log(user);
+
+
+
+
+// Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
+// Должно работать так:
+// let schedule = {};
+// alert(isEmpty(schedule)); // true
+// schedule["8:30"] = "get up";
+// alert(isEmpty(schedule)); // false
+// function isEmpty(obj) {
+//     for (let key in obj) {
+//         return false
+//     }
+//     return true
+// };
+
+
+
+// // У нас есть объект, в котором хранятся зарплаты нашей команды:
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// }
+// // Напишите код для суммирования всех зарплат и сохраните результат в переменной sum. Должно получиться 390.
+// // Если объект salaries пуст, то результат должен быть 0.
+// function sum(obj) {
+//     let sm = 0
+//     for (let key in obj) {
+//         sm += obj[key]
+//     }
+//     return sm
+// }
+// console.log(sum(salaries));
+
+
+
+
+// // Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
+// // Например:
+// // // до вызова функции
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+// };
+// function multiplyNumeric(obj) {
+//     for (let key in obj) {
+//         if (typeof obj[key] == 'number') {
+//             obj[key] *= 2
+//         }
+//     }
+// }
+// multiplyNumeric(menu);
+// console.log(menu);
+// // // после вызова функции
+// // menu = {
+// //   width: 400,
+// //   height: 600,
+// //   title: "My menu"
+// // };
+
+
+
+
+
+// Создайте объект calculator (калькулятор) с тремя методами:
+
+// read() (читать) запрашивает два значения и сохраняет их как свойства объекта.
+// sum() (суммировать) возвращает сумму сохранённых значений.
+// mul() (умножить) перемножает сохранённые значения и возвращает результат.
+// let calculator = {
+//   // ... ваш код ...
+// };
+
+
+// let calculator = {
+//     read() { this.a = +prompt('A?', 0), this.b = +prompt('B?', 0) },
+//     sum() { return this.a + this.b },
+//     mul() { return this.a * this.b },
+// };
+// calculator.read();
+// alert(calculator.sum());
+// alert(calculator.mul());
