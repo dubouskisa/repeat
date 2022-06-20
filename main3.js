ec4488168ddc0c3715aa4632050e5cc5a9cba852
Dubovski <dubouski.sa@gmail.com>
2022-06-20 19:40:05 +0300

    Update main3.js

--- main3.js
+++ main3.js
@@ -263,3 +263,134 @@
 // console.log(b);
 
 
+
+
+// Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
+// То есть дефисы удаляются, а все слова после них получают заглавную букву.
+// Примеры:
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+// P.S. Подсказка: используйте split, чтобы разбить строку на массив символов, потом переделайте всё как нужно и методом join соедините обратно.
+
+
+
+// function camelize(txt) {
+//     return txt.split('-').map((item, index) => index == 0 ? item : item[0].toUpperCase() + item.slice(1)).join('');
+// }
+// console.log(camelize("background-color"));
+// console.log(camelize("list-style-image"));
+// console.log(camelize("-webkit-transition"));
+
+
+
+
+// // Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет в нём элементы между a и b и отдаёт массив этих элементов.
+// // Функция должна возвращать новый массив и не изменять исходный.
+// // Например:
+// let arg1 = 1,
+//     arg2 = 4,
+//     arr = [5, 3, 8, 1];
+// // let filtered = filterRange(arr, 1, 4);
+// // alert( filtered ); // 3,1 (совпадающие значения)
+// // alert( arr ); // 5,3,8,1 (без изменений)
+
+// function filterRange(arr, a, b) {
+//     return arr.filter(item => item >= a && item < b);
+// }
+// console.log(arr);
+// console.log(filterRange(arr, arg1, arg2));
+
+
+
+
+
+
+// Напишите функцию filterRangeInPlace(arr, a, b), которая принимает массив arr и удаляет из него все значения кроме тех, которые находятся между a и b. То есть, проверка имеет вид a ≤ arr[i] ≤ b.
+// Функция должна изменять принимаемый массив и ничего не возвращать.
+// Например:
+// let arr = [5, 3, 8, 1];
+// filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+// alert( arr ); // [3, 1]
+
+// function filterRangeInPlace(arr, a, b) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] < a || arr[i] > b) {
+//             arr.splice(i, 1);
+//             i--;
+//         }
+//     }
+// }
+// filterRangeInPlace(arr, arg1, arg2);
+// console.log(arr);
+
+
+
+
+
+
+// Сортировать в порядке по убыванию
+// let arr = [5, 2, 1, -10, 8];
+// // ... ваш код для сортировки по убыванию
+// alert( arr ); // 8, 5, 2, 1, -10
+
+// let arr = [5, 2, 1, -10, 8];
+// function compareNumeric(a, b) {
+//     if (a > b) return -1;
+//     if (a == b) return 0;
+//     if (a < b) return 1;
+// };
+// arr = arr.sort(compareNumeric)
+// console.log(arr);  // 8, 5, 2, 1, -10
+
+
+
+
+// У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
+// Создайте функцию copySorted(arr), которая будет возвращать такую копию.
+// let arr = ["HTML", "JavaScript", "CSS"];
+// let sorted = copySorted(arr);
+// alert( sorted ); // CSS, HTML, JavaScript
+// alert( arr ); // HTML, JavaScript, CSS (без изменений)
+
+// let arr = ["HTML", "JavaScript", "CSS"];
+// let sorted = copySorted(arr);
+// function copySorted(arr) {
+//     return arr.slice().sort();
+// }
+// console.log(arr);
+// console.log(sorted);
+
+
+
+// Создайте функцию конструктор Calculator, которая создаёт «расширяемые» объекты калькулятора.
+// Задание состоит из двух частей.
+// Во-первых, реализуйте метод calculate(str), который принимает строку типа "1 + 2" в формате «ЧИСЛО оператор ЧИСЛО» (разделено пробелами) и возвращает результат. Метод должен понимать плюс + и минус -.
+// Пример использования:
+// let calc = new Calculator;
+// alert( calc.calculate("3 + 7") ); // 10
+
+// Затем добавьте метод addMethod(name, func), который добавляет в калькулятор новые операции. Он принимает оператор name и функцию с двумя аргументами func(a,b), которая описывает его.
+// Например, давайте добавим умножение *, деление / и возведение в степень **:
+
+// let powerCalc = new Calculator;
+// powerCalc.addMethod("*", (a, b) => a * b);
+// powerCalc.addMethod("/", (a, b) => a / b);
+// powerCalc.addMethod("**", (a, b) => a ** b);
+// let result = powerCalc.calculate("2 ** 3");
+// alert( result ); // 8
+// Для этой задачи не нужны скобки или сложные выражения.
+// Числа и оператор разделены ровно одним пробелом.
+// Не лишним будет добавить обработку ошибок.
+
+
+let calc = new Calculator;
+alert(calc.calculate("3 + 7")); // 10
+
+let powerCalc = new Calculator;
+powerCalc.addMethod("*", (a, b) => a * b);
+powerCalc.addMethod("/", (a, b) => a / b);
+powerCalc.addMethod("**", (a, b) => a ** b);
+let result = powerCalc.calculate("2 ** 3");
+alert(result); // 8
+
